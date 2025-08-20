501a352d745a831588d8be5b703f79e2480e72af
Sophiabarillas <20130303@colegioscj.edu.gt>
2025-08-20 10:57:33 -0600

    Login Final

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,7 +7,9 @@
     <title>Login</title>
 
     <!-- MDB icon -->
-    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <link rel="icon" 
+      href="<?= base_url('fotos/generated-image.png') ?>" 
+      type="image/png" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
     <!-- Google Fonts Roboto -->
@@ -22,18 +24,30 @@
             <div class="col col-xl-10">
                 <div class="card" style="border-radius: 1rem;">
                     <div class="row g-0">
-                        <div class="col-md-6 col-lg-5 d-none d-md-block">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                                 alt="login form"
-                                 class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                        </div>
-                        <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                            <div class="card-body p-4 p-lg-5 text-black">
+                        
+                        <section class="vh-100" style="background-color: #f5f5f5ff;">
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-xl-10">
+                        <div class="card" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            
+                            <div class="col-md-7 col-lg-5 d-flex align-items-center justify-content-center">
+                            <img src="<?= base_url('fotos/libros.png') ?>"
+                                alt="login form"
+                                class="img-fluid"
+                                style="border-radius: 1rem 0 0 1rem; max-width: 90%; max
+
+                        <div class="col-md-7  d-flex align-items-center">
+                            <div class="card-body p-4 p-lg-5 text-black w-100">
 
                                 <div class="d-flex align-items-center mb-3 pb-1">
-                                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                    <img src="<?= base_url('fotos/generated-image.png') ?>" 
+                                    alt="Logo EverBook" 
+                                    style="width: 40px; height: 40px; margin-right: 1rem;" />
                                     <span class="h1 fw-bold mb-0">EverBook</span>
                                 </div>
+
 
                                 <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Iniciar Sesión</h5>
 
